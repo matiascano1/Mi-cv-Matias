cb0e5d89edab38608c8632080026a3fe28925998
matiascano <matias.cano@hotmail.com>
2023-02-23 15:09:39 -0300

    mi primer commit

---
+++ script.js
@@ -0,0 +1,17 @@
+clr;
+let = menuVisible = false;
+
+function mostrarOcultarMenu() {
+  if (menuVisible) {
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+  } else {
+    document.getElementById("nav").classList = "responsive";
+    menuVisible = true;
+  }
+}
+
+function seleccionar() {
+  document.getElementById("nav").classList = "";
+  menuVisible = false;
+}
